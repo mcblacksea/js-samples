6162c004b703fb41cbf00039fd57a96b1464c0b9
Anton Odegov <altecsyst@gmail.com>
2020-05-31 02:44:45 +0300

    implemented classes ops

---
+++ js-ops/class-ops.js
@@ -0,0 +1,25 @@
+class BaseClass{
+    constructor(){
+        this.Name = "Base";
+    }
+
+    ShowClassName(){
+        console.log(this.Name);
+    }
+}
+
+class Secondary extends BaseClass{
+    constructor(){
+        super();
+        this.Name = "Secondary";
+        this.Layer = "Current";
+    }
+
+    ShowLayer(){
+        console.log(this.Layer);
+    }
+}
+
+const sec = new Secondary();
+sec.ShowClassName();
+sec.ShowLayer();
